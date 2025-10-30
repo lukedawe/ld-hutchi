9875a4c559d1e835abdb766e74c31dc33b246f40
Luke Dawe <luke.dawe@gmail.com>
2025-10-30 18:35:49 +0000

    Using `useFetch` from mantine instead of normal fetch requests.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -8,46 +8,14 @@ import '@mantine/core/styles.css';
 import CategoriesTable from './lib/ui/categories_table'
 import { useFetch } from '@mantine/hooks'
 
-
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 const API_BASE_URL = 'http://localhost:5173/services/v1'
 
-const FetchStatus = {
-  Loading: 'loading',
-  Success: 'success',
-  Error: 'error',
-} as const;
-
-type Status = typeof FetchStatus[keyof typeof FetchStatus]
-
 function App() {
-  // const [categories, setCategories] = useState<CategoryResponse[] | null>([]);
-  // const [status, setStatus] = useState<Status>(FetchStatus.Loading);
-  // id of expanded category (if you need to show/hide details)
-  // const [expandedCategoryId, setExpandedCategoryId] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
-
-  // const fetchCategories = async () => {
-  //   setStatus(FetchStatus.Loading);
-  //   setError(null);
-  //   const endpoint = `${API_BASE_URL}/breeds/categories/` + currentPage + "/" + 100;
-  //   fetch(endpoint)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setCategories(data);
-  //       console.log(data);
-  //     })
-  //     .then(() => setStatus(FetchStatus.Success))
-  //     .catch(err => {
-  //       console.error(`HTTP error; status: ${err}`);
-  //       setError("Endpoint not reachable.");
-  //       setCategories(null);
-  //     })
-  // }
-
   const { data, loading, error, refetch, abort } = useFetch<CategoryResponse[]>(
     `${API_BASE_URL}/breeds/categories/` + currentPage + "/" + 100,
     {autoInvoke: true}
